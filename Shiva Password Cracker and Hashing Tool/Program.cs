94fe44db3db2e0da64dfb140b632cab721ebbfea
Jake Rice <jake.rice@trojans.dsu.edu>
2022-11-17 12:04:41 -0800

    Add project files.

---
+++ Shiva Password Cracker and Hashing Tool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shiva_Password_Cracker_and_Hashing_Tool
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
