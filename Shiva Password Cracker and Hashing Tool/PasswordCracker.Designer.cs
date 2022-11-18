88cdf6646a86c204d8841466a790238e90c75c18
Jake Rice <jake.rice@trojans.dsu.edu>
2022-11-17 16:28:18 -0800

    File browser done

--- Shiva Password Cracker and Hashing Tool/PasswordCracker.Designer.cs
+++ Shiva Password Cracker and Hashing Tool/PasswordCracker.Designer.cs
@@ -33,13 +33,18 @@
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CipherText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pathText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(12, 27);
+            this.radioButton1.Location = new System.Drawing.Point(15, 30);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(51, 19);
             this.radioButton1.TabIndex = 0;
@@ -51,7 +56,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(12, 52);
+            this.radioButton2.Location = new System.Drawing.Point(15, 55);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(71, 19);
             this.radioButton2.TabIndex = 1;
@@ -63,7 +68,7 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(12, 77);
+            this.radioButton3.Location = new System.Drawing.Point(15, 80);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(71, 19);
             this.radioButton3.TabIndex = 2;
@@ -75,29 +80,78 @@
             // 
             this.radioButton4.AutoSize = true;
             this.radioButton4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(12, 102);
+            this.radioButton4.Location = new System.Drawing.Point(15, 105);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(68, 19);
+            this.radioButton4.Size = new System.Drawing.Size(71, 19);
             this.radioButton4.TabIndex = 3;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "RIPEMD";
+            this.radioButton4.Text = "SHA-512";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 15);
+            this.label1.Size = new System.Drawing.Size(95, 18);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Please select a hash format:";
+            this.label1.Text = "Select format:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(218, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hash:";
+            // 
+            // CipherText
+            // 
+            this.CipherText.Location = new System.Drawing.Point(134, 30);
+            this.CipherText.Name = "CipherText";
+            this.CipherText.Size = new System.Drawing.Size(207, 20);
+            this.CipherText.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(180, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select a word list:";
+            // 
+            // pathText
+            // 
+            this.pathText.Location = new System.Drawing.Point(132, 88);
+            this.pathText.Name = "pathText";
+            this.pathText.Size = new System.Drawing.Size(152, 20);
+            this.pathText.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 20);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Password_Cracker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 338);
+            this.ClientSize = new System.Drawing.Size(353, 171);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pathText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CipherText);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
@@ -119,5 +173,10 @@
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CipherText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pathText;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Shiva Password Cracker and Hashing Tool/PasswordCracker.cs
+++ Shiva Password Cracker and Hashing Tool/PasswordCracker.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,23 @@ namespace Shiva_Password_Cracker_and_Hashing_Tool
         private void Password_Cracker_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileBrowse = new OpenFileDialog();
+            fileBrowse.Filter = "Text files (*.txt)|*.txt"; // Only show txt files
+            if (fileBrowse.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pathText.Text = fileBrowse.FileName; 
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error: Please select a valid file");
+                }
+            }
         }
     }
 }
