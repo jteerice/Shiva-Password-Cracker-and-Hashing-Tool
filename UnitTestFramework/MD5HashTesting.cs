87c5e69073f90d320b14d30ad1504d47ef046570
Jake Rice <jake.rice@trojans.dsu.edu>
2022-12-07 10:02:14 -0800

    Working on unit testing

---
+++ UnitTestFramework/MD5HashTesting.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestFramework
+{
+    [TestClass]
+    public class MD5HashTesting
+    {
+        [TestMethod]
+        public void hash_String1()
+        {
+            string clearText = "phrase123456";
+            string solution = "6596088e10363a7093d725e76fe705f8";
+            MD5Hash md5Test = new MD5Hash(clearText);
+
+            string cipherText = md5Test.hash();
+
+            Assert.AreEqual(cipherText, solution);
+
+        }
+    }
+}
